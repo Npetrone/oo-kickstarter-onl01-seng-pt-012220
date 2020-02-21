4ba8ad8c7b4d03ebb03fd582f266278cc1b40b03
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-21 23:49:44 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,12 @@
 class Backer
   attr_accessor :name
   
-  @@backed_projects = []
+ # @@backed_projects = []
   
   def initialize(name)
     @name = name
-    
-    @@backed_projects << self
+    @@backed_projects = []
+    #@@backed_projects << self
   end
   
   
